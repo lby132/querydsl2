891e90a8b7dc53a76b585b4d9205e28dcf8e5a5a
lby132 <quddbscjswow@naver.com>
2022-12-09 01:17:28 +0900

    querydsl복습

---
+++ src/main/java/study/querydsl2/dto/UserDto.java
@@ -0,0 +1,10 @@
+package study.querydsl2.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+
+    private String name;
+    private int age;
+}
---
+++ src/test/java/study/querydsl2/Querydsl2ApplicationTests.java
@@ -0,0 +1,42 @@
+package study.querydsl2;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl2.entity.Hello;
+import study.querydsl2.entity.QHello;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Commit
+class Querydsl2ApplicationTests {
+
+	@Autowired
+	EntityManager em;
+
+	@Test
+	void contextLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+		final QHello qHello = new QHello("h");
+
+		final Hello result = query
+				.selectFrom(qHello)
+				.fetchOne();
+
+		assertThat(result).isEqualTo(hello);
+		assertThat(result.getId()).isEqualTo(hello.getId());
+	}
+
+}
